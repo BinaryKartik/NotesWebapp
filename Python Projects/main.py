c797c5ccb89b3b1d4ab632ca38734ef198fc497c
kartik-programmer <84560482+kartik-programmer@users.noreply.github.com>
2021-09-20 11:41:13 +0530

    Update main.py

--- Python Projects/main.py
+++ Python Projects/main.py
@@ -2,4 +2,4 @@ from logging import debug
 from Website import create_app
 app = create_app()
 if __name__ == '__main__':
-    app.run(debug=True) #debug for if we change the code this would rerun the server
+    app.run(debug=True)
